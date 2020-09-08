380d230cfa8d13c413d89d8af8d248de0f56f4da
JZJisawesome <JZJisawesome123@hotmail.com>
2020-09-08 11:51:43 -0400

    Update multest and jzjcorelib to do division

--- programs/c/jzjcorelib.h
+++ programs/c/jzjcorelib.h
@@ -7,9 +7,27 @@
 
 //JZJCoreLib custom functions
 
-//Needed by gcc
+//todo
+
+//GCC Integer Routines
+extern int __divsi3(int a, int b);//Not implemented
+extern long __divdi3(long a, long b);//Not implemented
+extern long long __divti3(long long a, long long b);//Not implemented
+
+extern __modsi3(int a, int b);//Not implemented
+extern long __moddi3(long a, long b);//Not implemented
+extern long long __modti3(long long a, long long b);//Not implemented
+
 extern int __mulsi3(int a, int b);
 extern long __muldi3(long a, long b);
-extern long long __multi3(long a, long b);//Not implemented
+extern long long __multi3(long long a, long long b);//Not implemented
+
+extern unsigned int __udivsi3(unsigned int a, unsigned int b);//Not implemented
+extern unsigned long __udivdi3(unsigned long a, unsigned long b);//Not implemented
+extern unsigned long long __udivti3(unsigned long long a, unsigned long long b);//Not implemented
+
+extern int __umodsi3(unsigned int a, unsigned int b);//Not implemented
+extern unsigned long __umoddi3(unsigned long a, unsigned long b);//Not implemented
+extern unsigned long long __umodti3(unsigned long long a, unsigned long long b);//Not implemented
 
 #endif
--- programs/c/programs/multest.c
+++ programs/c/programs/multest.c
@@ -1,9 +1,23 @@
 volatile int *port5Register = (int*)(0xFFFFFFF4);
 volatile int a = 20;//Volatile so it is not done at compile time
 volatile int b = 21;//Volatile so it is not done at compile time
+volatile unsigned int ua = 420;//Volatile so it is not done at compile time
+volatile unsigned int ub = 21;//Volatile so it is not done at compile time
 
 void main()
 {
-    *port5Register = a * b;//JZJCoreLib __mulsi3 function should return 420 (lol)
+    int result = a * b;//JZJCoreLib __mulsi3 function should return 420 (lol)
+    *port5Register = result;
+    
+    a = 100;
+    b = -100;
+    result = a * b;//-10000
+    *port5Register = result;
+    
+    //Testing unsigned division too
+    volatile unsigned int ua = 420;
+    result = ua / ub;
+    *port5Register = result;
+    
     return;
 }
