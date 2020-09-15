5530f67f1dad644b4a3dd99e69fb1c3ce5195460
JZJisawesome <JZJisawesome123@hotmail.com>
2020-09-14 21:59:44 -0400

    Make! Make! Such a beautiful thing! GNU Make makes me want to sing!

--- programs/c/programs/clibtest.c
+++ programs/c/programs/clibtest.c
@@ -10,6 +10,11 @@ void main()
 {
     volatile double testd1 = 2, testd2 = 16;
     //PORT5 = (int)(pow(testd1, testd2));
+    
+    volatile int test1 = 2, test2 = -16;
+    
+    PORT5 = abs(test1);
+    PORT5 = abs(test2);
 
     assert(PORT0 == 0);
     return;
