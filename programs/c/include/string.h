0c84bebca47579c06873b2635eb3e59c09827a84
JZJisawesome <JZJisawesome123@hotmail.com>
2020-09-21 22:27:38 -0400

    Implement memcmp and a better atoi

--- programs/c/include/string.h
+++ programs/c/include/string.h
@@ -11,9 +11,9 @@ size_t strxfrm(char* restrict dest, const char* restrict src, size_t count);
 
 //String Examination
 size_t strlen(const char* str);
-int strcmp(const char* lhs, const char* rhs);//TODO String.c
+int strcmp(const char* lhs, const char* rhs);//TODO string.S
 int strncmp(const char* lhs, const char* rhs, size_t count);//TODO String.c
-int strcoll(const char* lhs, const char* rhs);//TODO String.c (same implementation as strcmp probably)
+int strcoll(const char* lhs, const char* rhs);//TODO string.S
 char* strchr(const char* str, int ch);//TODO string.S
 char* strrchr(const char* str, int ch);//TODO string.S
 size_t strspn(const char* dest, const char *src);//TODO String.c
@@ -24,7 +24,7 @@ char* strtok(char* restrict str, const char* restrict delim);//TODO String.c
 
 //Character Array/Memory Manipulation
 void* memchr(const void* ptr, int ch, size_t count);//TODO string.S
-int memcmp(const void* lhs, const void* rhs, size_t count);//TODO String.c
+int memcmp(const void* lhs, const void* rhs, size_t count);//TODO string.S
 void* memset(void* dest, int ch, size_t count);
 void* memcpy(void* dest, const void* src, size_t count);
 void* memmove(void* dest, const void* src, size_t count);//TODO String.c
--- programs/c/src/libjzjcorec/String.c
+++ programs/c/src/libjzjcorec/String.c
@@ -24,7 +24,6 @@ inline static int isdigit_inline(int ch)
 
 //TODO lots left to implement
 
-//Idea of multiplying by 10 each time rather than an exponent function, using a char instead of an int literal for parsing (more readable), and reminder to stop if null is encountered thanks to: https://www.geeksforgeeks.org/write-your-own-atoi/
 //I came up with other ideas (including checking if characters are digits and parsing them in the same loop) myself
 int atoi(const char* str)//TODO make faster//TODO support sign before number
 {
@@ -33,22 +32,22 @@ int atoi(const char* str)//TODO make faster//TODO support sign before number
         ++str;//Shift the position of the string to access
         
     //str[0] now points to the first non-whitespace character
-    //Now we must find the number of characters the digit makes up
-    uint_fast8_t numberLength = 0;//Maximum of 10 decimal digits fit in an int
-    while (isdigit_inline(str[numberLength]))//Stop if we run out of digits
-        ++numberLength;//Add 1 for each digit we find
-        
-    //Now we now the length of digits to parse and the starting position (numberLength and str[0])
+    //We parse 1 digit at a time from this point on until we run out of digits
     int returnValue = 0;//Default value if numberLength == 0
     
-    for(uint_fast8_t i = 0; i < numberLength; ++i)//Count up to numberLength to parse each character
+    //Idea of multiplying by 10 each time rather than needing an exponent function, using a char instead of an int literal
+    //for parsing (more readable), and idea to parse without determining the length of the digits in str first
+    //thanks to: https://www.geeksforgeeks.org/write-your-own-atoi/
+    while (isdigit_inline(str[0]))
     {
-        //Multiply existing decimal digits in returnValue by 10
-        //Effectively shifting decimal places left rather than bits after each addition of a digit
+        //Multiply existing decimal digits in returnValue by 10 to shift them left
+        //Shifting _decimal_ places left rather than bits after each addition of a digit
         returnValue *= 10;//The compiler will translate this to (returnValue << 3) + (returnValue << 1)
         
-        int parsedCharacterValue = str[i] - '0';//Parse digit to a number
+        int parsedCharacterValue = str[0] - '0';//Parse digit to a number
         returnValue += parsedCharacterValue;//Add the next decimal digit to the value to be returned
+        
+        ++str;//Go to the next character
     }
     
     return returnValue;
